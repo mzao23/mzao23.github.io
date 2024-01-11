07f4b022e4ca2a943827db0a33355a2477e32dee
NizDevPy <nizaroxhxray@gmail.com>
2024-01-11 15:50:43 +0100

    addffiles

---
+++ db.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Nizar2007Nizq0000Aziz1960Amal1966.rickandmorty",
+    database="germun"
+)
+
+cursor = connection.cursor()
